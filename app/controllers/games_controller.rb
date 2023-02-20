7a456617d7a839c17bca32b028d6e7b34f944064
maxco2023 <maxime.coquerelle@gmail.com>
2023-02-20 17:08:17 +0100

    done

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,28 @@
+require "open-uri"
+require "json"
+
+class GamesController < ApplicationController
+  def new
+    @letters = []
+    10.times do
+      @letters.push(("A".."Z").to_a.sample)
+    end
+  end
+
+  def letter_in_grid(word, letters)
+    word.upcase.chars.all? { |letter| word.upcase.count(letter) <= letters.count(letter)}
+  end
+
+  def score
+      @letters = params[:letters]
+      @word = params[:word]
+      url = "https://wagon-dictionary.herokuapp.com/#{@word}"
+      word_dictionary = URI.open(url).read
+      worded = JSON.parse(word_dictionary)
+      if worded['found'] && letter_in_grid(@word, @letters)
+        @score = "winner, your word is valid"
+      else
+        @score = "Sorry, but #{@word}, can't be built out of #{@letters}"
+      end
+  end
+end

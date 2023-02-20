68c42adc56194174cb7dbd45f0e30c7907011cc6
maxco2023 <maxime.coquerelle@gmail.com>
2023-02-20 23:43:59 +0100

    done4

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -16,11 +16,13 @@ class GamesController < ApplicationController
   def score
     @letters = params[:letters]
     @word = params[:word]
+    @scoring = 0
     url = "https://wagon-dictionary.herokuapp.com/#{@word}"
     word_dictionary = URI.open(url).read
     worded = JSON.parse(word_dictionary)
     if worded['found'] && letter_in_grid(@word, @letters)
-      @score = "Congratulations! #{@word} is a valid English word"
+      @score = "Congratulations! #{@word.capitalize} is a valid English word"
+      @scoring += @word.length * 2
     else
       @score = "Sorry, but #{@word}, can't be built out of #{@letters}"
     end
